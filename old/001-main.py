b9ed212a3bf8a80e49cad2ad9f8e4a1e6e263a1f
Al Etienne <al@alandkimber.com>
2025-08-15 10:52:39 -0400

    cleanup files

--- old/001-main.py
+++
@@ -1,9 +0,0 @@
-# This program reads a text file and prints its content to the console.
-
-file_name = "frankenstein.txt"
-def get_book_text(file_name):
-      with open(file_name) as f:
-        file = f.read()
-        print(file)
-
-get_book_text("books/" + file_name)
